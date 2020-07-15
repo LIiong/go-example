6197ef3498fd63fb9a2cbde165706593823a4902
Liion <1121013687@qq.com>
2020-07-15 20:38:38 +0800

    add a friend

--- go_11.go
+++ go_11.go
@@ -7,6 +7,7 @@ import (
     "bytes"
     "regexp"
     "encoding/json"
+    "encoding/xml"
 )
 func Index(vs []string, t string) int {
     for i, v := range vs {
@@ -257,4 +258,46 @@ func main32() {
     enc := json.NewEncoder(os.Stdout)
     d := map[string]int{"apple": 5, "lettuce": 7}
     enc.Encode(d)
+}
+
+type Plant struct {
+    XMLName xml.Name `xml:"plant"`
+    Id      int      `xml:"id,attr"`
+    Name    string   `xml:"name"`
+    Origin  []string `xml:"origin"`
+}
+
+func (p Plant) String() string {
+    return fmt.Sprintf("Plant id=%v, name=%v, origin=%v",
+        p.Id, p.Name, p.Origin)
+}
+
+func main33() {
+    coffee := &Plant{Id: 27, Name: "Coffee"}
+    coffee.Origin = []string{"Ethiopia", "Brazil"}
+
+    out, _ := xml.MarshalIndent(coffee, " ", "  ")
+    fmt.Println(string(out))
+
+    fmt.Println(xml.Header + string(out))
+
+    var p Plant
+    if err := xml.Unmarshal(out, &p); err != nil {
+        panic(err)
+    }
+    fmt.Println(p)
+
+    tomato := &Plant{Id: 81, Name: "Tomato"}
+    tomato.Origin = []string{"Mexico", "California"}
+
+    type Nesting struct {
+        XMLName xml.Name `xml:"nesting"`
+        Plants  []*Plant `xml:"parent>child>plant"`
+    }
+
+    nesting := &Nesting{}
+    nesting.Plants = []*Plant{coffee, tomato}
+
+    out, _ = xml.MarshalIndent(nesting, " ", "  ")
+    fmt.Println(string(out))
 }
